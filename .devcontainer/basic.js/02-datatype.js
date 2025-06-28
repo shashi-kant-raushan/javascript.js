ceba52f64a1dfb8c4b0cc3d0b319cd0ed805ec75
Shashikant Raushan <shashikantraushan9@gmail.com>
2025-06-28 10:00:01 +0000

    datatype

---
+++ .devcontainer/basic.js/02-datatype.js
@@ -0,0 +1,8 @@
+"use strict";// treats aal js code as newer version
+//alert(3 +3) we are using nodejs, not browser
+
+console.log(4+
+    5) // focus on the code readibilitry that should be high
+console.log("shashi")
+console.log(typeof undifined);//undifined
+console.log(typeof null)// object
---
+++ .devcontainer/basic.js/variable.js
@@ -0,0 +1,15 @@
+const accountId =12312
+let accountEmail ="shashikantraushan9@gmail.com"
+var accountPassword = "12345"
+accountCity = "patna"
+let accountState;
+//accountId = 2 //not allowed
+
+
+accountEmail ="asdd@.com"
+accountPassword ="3435"
+accountCity ="punjab"
+console.log(accountId);
+
+/*let is better than var  prefer not to use var because of issue in block space*/
+console.table([accountId,accountCity,accountEmail,accountPassword,accountState])
